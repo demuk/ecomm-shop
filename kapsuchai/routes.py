ac3af1fb510d60105cf2243a8482636c1cfb5459
Kiptoo Dennis <denysmutay@gmail.com>
2022-03-14 23:16:17 +0600

    routes for home, account and about pages

---
+++ kapsuchai/routes.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, url_for, redirect, flash, request
+from kapsuchai import app
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', title='home')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='about')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='login')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title='register')
+
+
+@app.route('/account')
+def account():
+    return render_template('account.html', title='account')
