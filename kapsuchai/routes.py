708fc38ad3cc8a36b0600242a689fd36d005acea
Kiptoo Dennis <denysmutay@gmail.com>
2022-03-28 16:48:02 +0600

    improvements to add to cart route

--- kapsuchai/routes.py
+++ kapsuchai/routes.py
@@ -1,11 +1,8 @@
-import os
-import secrets
-from flask import render_template, url_for, redirect, flash, request
-from flask_login import login_user, current_user, logout_user, login_required
-from kapsuchai import app, bcrypt, db
-from kapsuchai.models import User,Products, Cart
+from flask import render_template, url_for, flash, redirect, request
+from kapsuchai import app, db, bcrypt
 from kapsuchai.forms import RegistrationForm, LoginForm, UpdateAccountForm
-from sqlalchemy import func
+from kapsuchai.models import User, Products, Cart
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 def getLoginDetails():
@@ -16,25 +13,32 @@ def getLoginDetails():
     return noOfItems
 
 
-@app.route("/logout")
-def logout():
-    logout_user()
-    return redirect(url_for('home'))
-
-
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def home():
+    title = 'home'
     noOfItems = getLoginDetails()
-    return render_template('home.html', title='home', noOfItems=noOfItems)
+    return render_template('home.html', noOfItems=noOfItems, title=title)
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html', title='about')
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    title = 'register'
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(lastname=form.lastname.data, firstname=form.firstname.data, email=form.email.data,
+                    password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
@@ -50,72 +54,19 @@ def login():
     return render_template('login.html', title='Login', form=form)
 
 
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(lastname=form.lastname.data,firstname=form.firstname.data,email=form.email.data, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Your account has been created!', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='register', form=form)
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 
-@app.route("/select_products", methods=['GET', 'POST'])
-def select_products():
-    title = 'select products'
-    noOfItems = getLoginDetails()
-    products = Products.query.all()
-    return render_template('select_products.html', products=products,noOfItems=noOfItems, title=title)
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
 
 
-@app.route("/addToCart/<int:product_id>")
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
-def addToCart(product_id):
-    # check if product is already in cart
-    row = Cart.query.filter_by(product_id=product_id, buyer=current_user).first()
-    if row:
-        # if in cart update quantity : +1
-        row.quantity += 1
-        db.session.commit()
-        flash('This item is already in your cart, 1 quantity added!', 'success')
-
-        # if not, add item to cart
-    else:
-        user = User.query.get(current_user.id)
-        user.add_to_cart(product_id)
-    return redirect(url_for('select_products'))
-
-
-@app.route("/cart", methods=["GET", "POST"])
-@login_required
-def cart():
-    title = 'Cart'
-    noOfItems = getLoginDetails()
-    # display items in cart
-    cart = Products.query.join(Cart).add_columns(Cart.quantity, Products.price, Products.name, Products.id).filter_by(buyer=current_user).all()
-    subtotal = 0
-    for item in cart:
-        subtotal+=int(item.price)*int(item.quantity)
-
-    if request.method == "POST":
-        qty = request.form.get("qty")
-        idpd = request.form.get("idpd")
-        cartitem = Cart.query.filter_by(product_id=idpd).first()
-        cartitem.quantity = qty
-        db.session.commit()
-        cart = Products.query.join(Cart).add_columns(Cart.quantity, Products.price, Products.name, Products.id).filter_by(buyer=current_user).all()
-        subtotal = 0
-        for item in cart:
-            subtotal+=int(item.price)*int(item.quantity)
-    return render_template('cart.html', cart=cart, noOfItems=noOfItems, subtotal=subtotal, title=title)
-
-
-@app.route('/account', methods=['GET', 'POST'])
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
@@ -129,4 +80,63 @@ def account():
         form.lastname.data = current_user.lastname
         form.firstname.data = current_user.firstname
         form.email.data = current_user.email
-    return render_template('account.html', title='Account',form=form)
+    return render_template('account.html', title='Account',
+                           form=form)
+
+
+@app.route("/select_products", methods=['GET', 'POST'])
+def select_products():
+    noOfItems = getLoginDetails()
+    products = Products.query.all()
+    return render_template('select_products.html', products=products, noOfItems=noOfItems, title='select products')
+
+
+@app.route("/addToCart/<int:product_id>")
+@login_required
+def addToCart(product_id):
+    # check if product is already in cart
+    row = Cart.query.filter_by(product_id=product_id, buyer=current_user).first()
+    if row:
+        # if in cart update quantity : +1
+        row.quantity += 1
+        db.session.commit()
+        flash('This item is already in your cart, 1 quantity added!', 'success')
+
+
+    else:
+        user = User.query.get(current_user.id)
+        user.add_to_cart(product_id)
+    return redirect(url_for('select_products'))
+
+
+@app.route("/cart", methods=["GET", "POST"])
+@login_required
+def cart():
+    noOfItems = getLoginDetails()
+    # display items in cart
+    cart = Products.query.join(Cart).add_columns(Cart.quantity, Products.price, Products.name, Products.id).filter_by(buyer=current_user).all()
+    subtotal = 0
+    for item in cart:
+        subtotal += int(item.price) * int(item.quantity)
+
+    if request.method == "POST":
+        qty = request.form.get("qty")
+        idpd = request.form.get("idpd")
+        cartitem = Cart.query.filter_by(product_id=idpd).first()
+        cartitem.quantity = qty
+        db.session.commit()
+        cart = Products.query.join(Cart).add_columns(Cart.quantity, Products.price, Products.name,Products.id).filter_by(buyer=current_user).all()
+        subtotal = 0
+        for item in cart:
+            subtotal += int(item.price) * int(item.quantity)
+    return render_template('cart.html', cart=cart, noOfItems=noOfItems, subtotal=subtotal, title='cart')
+
+
+@app.route("/removeFromCart/<int:product_id>")
+@login_required
+def removeFromCart(product_id):
+    item_to_remove = Cart.query.filter_by(product_id=product_id, buyer=current_user).first()
+    db.session.delete(item_to_remove)
+    db.session.commit()
+    flash('Your item has been removed from your cart!', 'success')
+    return redirect(url_for('cart'))
