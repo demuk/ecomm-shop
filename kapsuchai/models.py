b30d5e18fc9292d4fb26e44ba22882b1e8a7cf46
Kiptoo Dennis <denysmutay@gmail.com>
2022-07-11 01:47:06 +0300

    added column for product image

--- kapsuchai/models.py
+++ kapsuchai/models.py
@@ -31,7 +31,8 @@ class Products(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.Text, nullable=False)
+    product_image = db.Column(db.String())
+    description = db.Column(db.String(), nullable=False)
 
     def __repr__(self):
         return f"Products('{self.name}', '{self.price}')"
